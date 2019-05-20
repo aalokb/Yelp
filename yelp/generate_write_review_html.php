e873df62c346fb9f972149b14af69c436165afc2
NikhilAnand1998 <nikhilanand249@gmail.com>
2019-05-19 21:30:49 -0700

    Fixed generate_write_review_html

--- yelp/generate_write_review_html.php
+++ yelp/generate_write_review_html.php
@@ -1,5 +1,18 @@
 
 <?php
+
+$host = 'classmysql.engr.oregonstate.edu';
+$db = 'cs340_anandn';
+$user = 'cs340_anandn';
+$charset = 'utf8mb4';
+$pass = 4016;
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$opt = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
+
 ///////////////////GENERATE DYNAMIC HTML///////////////////////////////////////////////////////
   // generate the HTML based off of extraction query:
 
@@ -10,29 +23,31 @@
         User ID: <input type="text" name="user_id"><br>
         Restaurant ID:
         <select name="restaurant_id">
-    HTMLINPUT;
+HTMLINPUT;
 
      //QUERY THEN ECHO RESULTS HERE
     $pdo = new PDO($dsn, $user, $pass, $opt);
     $res = $pdo->prepare("SELECT id FROM restaurant");
-    foreach($res as $row){      
+    $res->execute();
+    foreach($res as $row){
           echo <<<HTMLINPUT
             <option value="{$row["id"]}"> restaurant {$row['id']} </option>
-          HTMLINPUT;
+
+HTMLINPUT;
     }
 
-    echo "</select><br>"
+    echo "</select><br>";
 
     echo <<<HTMLINPUT
     Review: <input type="textarea" name="content"><br>
-    
+
     Rating: <input type="number" name="rating"><br>
 
     Price (1-3):
     <input type="number" name="price"><br>
     <input type="submit" name="write_review_submit" value="Submit">
   </form>
-    HTMLINPUT;
+HTMLINPUT;
 
 
 /////////////////////END HTML GENERATION///////////////////////////////////////////////////////
